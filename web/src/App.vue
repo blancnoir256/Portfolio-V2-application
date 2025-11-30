e90e5b329b01c76cf8dd7cfb8269758e53b30dfc
blancnoir256 <116483234+blancnoir256@users.noreply.github.com>
2025-11-30 19:49:59 +0900

    fix: HTML言語属性を日本語に設定し、タイトルをBN256ポートフォリオに変更

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>This is BN256's portfolio site.</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    メンテナンス中です。
   </p>
 </template>
 
