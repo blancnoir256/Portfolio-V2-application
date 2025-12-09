1041085b9cdcfdc2a50aa6192f3dbfaabd4df5e9
blancnoir256 <116483234+blancnoir256@users.noreply.github.com>
2025-12-10 00:49:09 +0900

    feat githubのリポジトリを載せた

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,6 +4,8 @@
   <h1>This is BN256's portfolio site.</h1>
   <p>
     メンテナンス中です。
+    目下制作中なので今しばらくお待ち下さい。
+    <a href="https://github.com/blancnoir256/Portfolio-V2-application">githubリポジトリ</a>
   </p>
 </template>
 
